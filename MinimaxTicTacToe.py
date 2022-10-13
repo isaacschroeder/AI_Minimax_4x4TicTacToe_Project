53b624636e644c6bd721116fcc947382def12ddf
Teddy <teddykucaba@yahoo.com>
2022-10-13 18:38:36 -0500

    generate possible plays and alter Tile enum to represent playable tiles

--- MinimaxTicTacToe.py
+++ MinimaxTicTacToe.py
@@ -7,6 +7,7 @@ class Tile(Enum):
     EMPTY = 0
     X = 1
     O = 2
+    PLAYABLE = 3
 
 class Player(Enum):
     PLAYER_1 = 0
@@ -41,11 +42,33 @@ class Game:
     # empty space next to an existing piece horizontally,
     # vertically, or diagonally on the board."
     # Currently assumes valid play is made.
+    def generatePossiblePlays(self):
+        return [Position(row,col) for row in range(1,self.row_count) for col in range(1,self.col_count) if self.board[row][col] == Tile.PLAYABLE]
+
     def makePlay(self, player, position):
+        r = position.row
+        c = position.col
         if player == Player.PLAYER_1:
-            self.board[position.row][position.col] = self.p1_tile
+            self.board[r][c] = self.p1_tile
         else:
-            self.board[position.row][position.col] = self.p2_tile
+            self.board[r][c] = self.p2_tile
+
+        if self.board[r-1][c-1] == Tile.EMPTY and r>0 and c>0: #up and left
+            self.board[r-1][c-1] = Tile.PLAYABLE
+        elif self.board[r-1][c] == Tile.EMPTY and r > 0: #up
+            self.board[r-1][c] == Tile.PLAYABLE
+        elif self.board[r-1][c+1] == Tile.EMPTY and r > 0 and c <=5: #up and right
+            self.board[r-1][c+1] == Tile.PLAYABLE
+        elif self.board[r][c-1] == Tile.EMPTY and c > 0: #left
+            self.board[r][c-1] == Tile.PLAYABLE
+        elif self.board[r][c+1] == Tile.EMPTY and c <= 5: #right
+            self.board[r][c+1] == Tile.PLAYABLE
+        elif self.board[r+1][c-1] == Tile.EMPTY and r <= 4 and c>0: #down and left
+            self.board[r+1][c-1] == Tile.PLAYABLE
+        elif self.board[r+1][c] == Tile.EMPTY and r <= 4: #down 
+            self.board[r+1][c] == Tile.PLAYABLE
+        elif self.board[r+1][c+1] == Tile.EMPTY and r <= 4 and c <=5: #down and right
+            self.board[r+1][c+1] == Tile.PLAYABLE
 
     def getBoard(self):
         return self.board
